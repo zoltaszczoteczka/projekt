550ad0fbc13694d8673fc4c5b1699f084ef9d901
zoltaszczoteczka <kperon@o2.pl>
2019-01-20 13:43:44 +0100

    zepsute dodawanie

--- views/AdminController/index.php
+++ views/AdminController/index.php
@@ -43,11 +43,11 @@
     <?php endif; ?>
 
 
-    <div class="adminAddItem" action="?page=adminAddItem" method="POST">
-        <br><label for="name"></label><input name="name" value="<?php if(isset($_POST['name']) && !preg_match('/[^A-Za-z]/', $_POST['name'])) echo $_POST['name'] ?>" placeholder="name" required/><label for="price"></label><input name="price" value="<?php if(isset($_POST['price']) && !preg_match('/[^1-9]/', (int)$_POST['price'])) echo (int)$_POST['price'] ?>" placeholder="price" required/>
+    <form class="adminAddItem" action="?page=adminAddItem" method="POST">
+        <br><label for="name"></label><input name="name" value="<?php if(isset($_POST['name']) && !preg_match('/[^A-Za-z]/', $_POST['name'])) echo $_POST['name'] ?>" placeholder="name" required/>
+        <label for="price"></label><input name="price" value="<?php if(isset($_POST['price']) && !preg_match('/[^1-9]/', (int)$_POST['price'])) echo (int)$_POST['price'] ?>" placeholder="price" required/>
         <input type="submit" value="Add Item" class="btn btn-warning btn-sm"/>
-        </form>
-
+    </form>
 
     <div class="row">
             <h4 class="mt-4">Equipment:</h4>
